c07cb900b35282f274d5f67f12082776f96a6ce2
wim forton <wim.forton@gmail.com>
2024-01-11 19:22:40 +0100

    Add project files.

---
+++ wimfortonMVC/Controllers/ResumeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace wimfortonMVC.Controllers
+{
+    public class ResumeController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            ViewData["NumTextblokken"] = 5;
+            ViewData["Titel"] = "Titel inhoud";
+            ViewData["Textblok"] = "Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud Textblok inhoud";
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
