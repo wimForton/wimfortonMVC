63f3637cee78c8820c48877a737c3ec07e8f34bf
wim forton <wim.forton@gmail.com>
2024-02-19 19:42:04 +0100

    2 project pages added

---
+++ wimfortonMVC/Controllers/ProjectsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+
+namespace wimfortonMVC.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Midi()
+        {
+            return View();
+        }
+        public IActionResult Framework3D()
+        {
+            return View();
+        }
+
+
+    }
+}
